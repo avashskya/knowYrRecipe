e1509966e2c357873ce1d3a63fcd027625ffee29
avashskya <avashshakya@yandex.com>
2021-04-08 12:23:17 +0545

    view

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+9.times do |i|
+    Recipe.create(
+      name: "Recipe #{i + 1}",
+      ingredients: '25g butter, 1 tsp cornflour,100g parmesan, grated nutmeg, 250g fresh fettuccine or tagliatelle, snipped chives or chopped parsley to serve (optional)',
+      process: 'In a pan, stir the butter and cornflour over a low heat and bring to a low simmer. Turn off the heat and keep warm.'
+    )
+  end
